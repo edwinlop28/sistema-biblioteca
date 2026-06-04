2cb1bde65ae88f8b7f75bb4b95b68510cd514fc7
Edwin <2811manotas@gmail.com>
2026-06-04 11:30:41 -0500

    ultimo cambio

--- persona.py
+++ persona.py
@@ -6,9 +6,6 @@ class Persona():
         self.__cedula = cedula
         self._activo = True
 
-    def get_id(self):
-        return self.__id
-
     def get_nombre(self):
         return self.__nombre
 
