1966c319e6b00547723225656347de4df903be13
Edwin <2811manotas@gmail.com>
2026-06-04 12:46:54 -0500

    try and except

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3
 from cliente import Cliente
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 bcrypt = Bcrypt()
 
@@ -70,101 +71,136 @@ class Data_Base:
         print("Tablas creadas ")
 
     def crear_admin_inicial(self):
-        self.__cursor.execute(
-            "SELECT * FROM empleados WHERE rol = ?",
-            ("admin",)
-        )
-        password_admin = bcrypt.generate_password_hash("200728").decode("utf-8")
-
-        if self.__cursor.fetchone() is None:
+        try:
             self.__cursor.execute(
-                """INSERT INTO empleados 
-                (nombre, cedula, email, password, turno, rol) VALUES (?, ?, ?, ?, ?, ?)""",
-
-                ("Edwin López","1065880632","edwin@mail.com",password_admin,"mañana","admin")
+                "SELECT * FROM empleados WHERE rol = ?",
+                ("admin",)
             )
 
-            self.__conexion.commit()
+            password_admin = bcrypt.generate_password_hash("200728").decode("utf-8")
+
+            if self.__cursor.fetchone() is None:
+                self.__cursor.execute(
+                    """INSERT INTO empleados
+                    (nombre, cedula, email, password, turno, rol)
+                    VALUES (?, ?, ?, ?, ?, ?)""",
+                    ("Edwin López", "1065880632", "edwin@mail.com",
+                    password_admin, "mañana", "admin")
+                )
+                self.__conexion.commit()
+        except Exception as e:
+            print(f"Error al crear administrador inicial: {e}")
+            raise
 
     def insertar_empleado(self, empleado):
-        self.__cursor.execute(
-            "SELECT * FROM empleados WHERE email = ?",
-            (empleado.get_email(),)
-        )
+        try:
+            self.__cursor.execute(
+                "SELECT * FROM empleados WHERE email = ?",
+                (empleado.get_email(),)
+            )
+            
+            if self.__cursor.fetchone() is None:
+                self.__cursor.execute("""
+                INSERT INTO empleados 
+                (nombre, email, password, cedula, turno, rol) 
+                VALUES (?, ?, ?, ?, ?, ?)
+                """, (
+                    empleado.get_nombre(),
+                    empleado.get_email(),
+                    empleado.get_password(),
+                    empleado.get_cedula(),
+                    empleado.get_turno(),
+                    empleado.get_rol()
+                ))
+
+                self.__conexion.commit()
+                return True
+
+            return False
+
+        except Exception as e:
+            print(f"Error al insertar empleado: {e}")
+            return False
         
-        if self.__cursor.fetchone() is None:
+    def insertar_cliente(self, cliente):
+        try:
             self.__cursor.execute("""
-            INSERT INTO empleados 
-            (nombre, email, password,cedula, turno, rol) 
-            VALUES (?, ?, ?, ?, ?, ?)
+            INSERT INTO clientes
+            (nombre, email, cedula, telefono, direccion)
+            VALUES (?, ?, ?, ?, ?)
             """, (
-                empleado.get_nombre(),
-                empleado.get_email(),
-                empleado.get_password(),
-                empleado.get_cedula(),
-                empleado.get_turno(),
-                empleado.get_rol()
+                cliente.get_nombre(),
+                cliente.get_email(),
+                cliente.get_cedula(),
+                cliente.get_telefono(),
+                cliente.get_direccion()
             ))
+
             self.__conexion.commit()
+            print(f"Cliente '{cliente.get_nombre()}' guardado en DB")
             return True
 
-        return False
-
-    def insertar_cliente(self,cliente):
-        self.__cursor.execute("""
-        INSERT INTO clientes
-        (nombre, email, cedula, telefono, direccion) 
-        VALUES (?, ?, ?, ?, ?)
-        """, (
-            cliente.get_nombre(),
-            cliente.get_email(),
-            cliente.get_cedula(),
-            cliente.get_telefono(),
-            cliente.get_direccion()
-        ))
-        self.__conexion.commit()
-        print(f"Cliente '{cliente.get_nombre()}' guardado en DB ")
+        except Exception as e:
+            print(f"Error al insertar cliente: {e}")
+            return False
 
     def insertar_libro(self, libro):
-        self.__cursor.execute(
-            "SELECT * FROM libros WHERE isbn = ?",
-            (libro.get_isbn(),)
-        )
-        if self.__cursor.fetchone() is None:
-            self.__cursor.execute("""
-                INSERT INTO libros
-                (isbn, titulo, autor, editorial, año, cantidad, disponibles)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-            """, (
-                libro.get_isbn(),
-                libro.get_titulo(),
-                libro.get_autor(),
-                libro.get_editorial(),
-                libro.get_año(),
-                libro.get_cantidad(),
-                libro.get_disponibles()
-            ))
-            self.__conexion.commit()
-            print(f"Libro '{libro.get_titulo()}' guardado en DB ")
-        else:
-            print(f"Libro '{libro.get_titulo()}' ya existe ")
+        try:
+            self.__cursor.execute(
+                "SELECT * FROM libros WHERE isbn = ?",
+                (libro.get_isbn(),)
+            )
+
+            libro_db = self.__cursor.fetchone()
+
+            if not libro_db:
+                self.__cursor.execute("""
+                    INSERT INTO libros
+                    (isbn, titulo, autor, editorial, año, cantidad, disponibles)
+                    VALUES (?, ?, ?, ?, ?, ?, ?)
+                """, (
+                    libro.get_isbn(),
+                    libro.get_titulo(),
+                    libro.get_autor(),
+                    libro.get_editorial(),
+                    libro.get_año(),
+                    libro.get_cantidad(),
+                    libro.get_disponibles()
+                ))
+
+                self.__conexion.commit()
+                print(f"Libro '{libro.get_titulo()}' guardado en DB")
+                return True
+
+            return False
+
+        except Exception as e:
+            print(f"Error al insertar libro: {e}")
+            return False
 
     def insertar_prestamo(self, prestamo):
-        self.__cursor.execute("""
-            INSERT INTO prestamos 
-            (isbn_libro, cedula_cliente, email_empleado_presto, 
-            fecha_prestamo, fecha_vencimiento, estado) 
-            VALUES (?, ?, ?, ?, ?, ?)
-        """, (
-            prestamo.get_libro().get_isbn(),
-            prestamo.get_cliente().get_cedula(),
-            prestamo.get_empleado_presto().get_email(),
-            prestamo.get_fecha_prestamo(),
-            prestamo.get_fecha_vencimiento(),
-            prestamo.get_estado()
-        ))
-        self.__conexion.commit()
-        print(f"Préstamo guardado en DB ")
+        try:
+            self.__cursor.execute("""
+                INSERT INTO prestamos
+                (isbn_libro, cedula_cliente, email_empleado_presto,
+                fecha_prestamo, fecha_vencimiento, estado)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (
+                prestamo.get_libro().get_isbn(),
+                prestamo.get_cliente().get_cedula(),
+                prestamo.get_empleado_presto().get_email(),
+                prestamo.get_fecha_prestamo(),
+                prestamo.get_fecha_vencimiento(),
+                prestamo.get_estado()
+            ))
+
+            self.__conexion.commit()
+            print("Préstamo guardado en DB")
+            return True
+
+        except Exception as e:
+            print(f"Error al guardar préstamo: {e}")
+            return False
     
     def obtener_empleados(self):
         self.__cursor.execute("SELECT * FROM empleados")
@@ -172,9 +208,9 @@ class Data_Base:
     
     def obtener_clientes_db(self):
        clientes = []
-       Rows = self.__cursor.execute("SELECT * FROM clientes").fetchall()
+       rows = self.__cursor.execute("SELECT * FROM clientes").fetchall()
        
-       for row in Rows:
+       for row in rows:
             cliente = Cliente(row[0],row[1], row[2], row[3], row[4], row[5])
             clientes.append(cliente) 
        return clientes
@@ -232,19 +268,26 @@ class Data_Base:
         return None
     
     def cerrar_prestamo(self, id_prestamo, email_empleado):
-        from datetime import datetime
-        self.__cursor.execute("""
-            UPDATE prestamos 
-            SET estado = 'devuelto',
-                email_empleado_recibio = ?,
-                fecha_devolucion = ?
-            WHERE id = ?
+
+        try:
+            self.__cursor.execute("""
+                UPDATE prestamos
+                SET estado = 'devuelto',
+                    email_empleado_recibio = ?,
+                    fecha_devolucion = ?
+                WHERE id = ?
             """, (
-            email_empleado,
-            datetime.now().strftime('%Y-%m-%d %H:%M'),
-            id_prestamo
-        ))
-        self.__conexion.commit()
+                email_empleado,
+                datetime.now().strftime('%Y-%m-%d %H:%M'),
+                id_prestamo
+            ))
+
+            self.__conexion.commit()
+            return True
+
+        except Exception as e:
+            print(f"Error al cerrar préstamo: {e}")
+            return False
 
     def obtener_prestamos_activos(self):
         self.__cursor.execute("""
@@ -254,19 +297,35 @@ class Data_Base:
         return self.__cursor.fetchall()
     
     def restar_disponible(self, isbn):
-        self.__cursor.execute("""
-            UPDATE libros SET disponibles = disponibles - 1
-            WHERE isbn = ?
-        """, (isbn,))
-        self.__conexion.commit()
+        try:
+            self.__cursor.execute("""
+                UPDATE libros
+                SET disponibles = disponibles - 1
+                WHERE isbn = ?
+            """, (isbn,))
+
+            self.__conexion.commit()
+            return True
+
+        except Exception as e:
+            print(f"Error al restar disponible: {e}")
+            return False
 
     def sumar_disponible(self, isbn):
-        self.__cursor.execute("""
-            UPDATE libros SET disponibles = disponibles + 1
-            WHERE isbn = ?
-        """, (isbn,))
-        self.__conexion.commit()
+        try:
+            self.__cursor.execute("""
+                UPDATE libros
+                SET disponibles = disponibles + 1
+                WHERE isbn = ?
+            """, (isbn,))
 
+            self.__conexion.commit()
+            return True
+
+        except Exception as e:
+            print(f"Error al sumar disponible: {e}")
+            return False
+        
     def obtener_isbn_prestamo(self, id_prestamo):
         self.__cursor.execute(
             "SELECT isbn_libro FROM prestamos WHERE id = ?",
@@ -282,12 +341,20 @@ class Data_Base:
         """, (isbn,))
         return self.__cursor.fetchall()
     
-    def eliminar_libro(self,isbn):
-        self.__cursor.execute("""
-            DELETE FROM libros WHERE isbn = ?
-        """, (isbn,))
-        self.__conexion.commit()
-        print(f"Libro con ISBN '{isbn}' eliminado de DB ")
+    def eliminar_libro(self, isbn):
+        try:
+            self.__cursor.execute("""
+                DELETE FROM libros
+                WHERE isbn = ?
+            """, (isbn,))
+
+            self.__conexion.commit()
+            print(f"Libro con ISBN '{isbn}' eliminado de DB")
+            return True
+
+        except Exception as e:
+            print(f"Error al eliminar libro: {e}")
+            return False
 
     def existe_libro(self,isbn):
         self.__cursor.execute(
